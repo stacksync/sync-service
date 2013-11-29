4a13367c519fc1455a89e20e1b29fbe026d65ccf
Adrian Moreno <adrian.moreno@urv.cat>
2013-11-29 16:03:01 +0100

    created daemon class and init script

---
+++ src/main/java/com/stacksync/syncservice/SyncServiceDaemon.java
@@ -0,0 +1,154 @@
+package com.stacksync.syncservice;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import omq.common.broker.Broker;
+
+import org.apache.commons.daemon.Daemon;
+import org.apache.commons.daemon.DaemonContext;
+import org.apache.commons.daemon.DaemonInitException;
+import org.apache.log4j.Logger;
+
+import com.stacksync.syncservice.db.ConnectionPool;
+import com.stacksync.syncservice.db.ConnectionPoolFactory;
+import com.stacksync.syncservice.exceptions.DAOConfigurationException;
+import com.stacksync.syncservice.omq.ISyncService;
+import com.stacksync.syncservice.omq.SyncServiceImp;
+import com.stacksync.syncservice.rpc.XmlRpcSyncHandler;
+import com.stacksync.syncservice.rpc.XmlRpcSyncServer;
+import com.stacksync.syncservice.util.Config;
+import com.stacksync.syncservice.util.Constants;
+
+public class SyncServiceDaemon implements Daemon {
+
+	private static final Logger logger = Logger
+			.getLogger(SyncServiceDaemon.class.getName());
+	private static ConnectionPool pool = null;
+	private static XmlRpcSyncServer xmlRpcServer = null;
+	private static Broker broker = null;
+	private static SyncServiceImp syncService = null;
+
+	
+
+	@Override
+	public void init(DaemonContext dc) throws DaemonInitException, Exception {
+		logger.info(String.format("Initializing StackSync server v%s...",
+				SyncServiceDaemon.getVersion()));
+
+		try {
+			String[] argv = dc.getArguments();
+			
+			if(argv.length == 0 ){
+				logger.error("No config file passed to StackSync server.");
+				System.exit(1);
+			}
+			
+			String configPath = argv[0];
+
+			File file = new File(configPath);
+			if (!file.exists()) {
+				logger.error("'" + configPath + "' file not found");
+				System.exit(2);
+			}
+
+			Config.loadProperties(configPath);
+
+		} catch (IOException e) {
+			logger.error("Could not load properties file.", e);
+			System.exit(7);
+		}
+
+		try {
+
+			String datasource = Config.getDatasource();
+			pool = ConnectionPoolFactory.getConnectionPool(datasource);
+
+			// it will try to connect to the DB, throws exception if not
+			// possible.
+			Connection conn = pool.getConnection();
+			conn.close();
+
+			logger.info("Connection to database succeded");
+		} catch (DAOConfigurationException e) {
+			logger.error("Connection to database failed.", e);
+			System.exit(3);
+		} catch (SQLException e) {
+			logger.error("Connection to database failed.", e);
+			System.exit(4);
+		}
+
+		logger.info("Initializing the messaging middleware...");
+		try {
+			broker = new Broker(Config.getProperties());
+			syncService = new SyncServiceImp(broker, pool);
+			logger.info("Messaging middleware initialization succeeded");
+		} catch (Exception e) {
+			logger.error("Could not initialize ObjectMQ.", e);
+			System.exit(5);
+		}
+	}
+
+	@Override
+	public void start() throws Exception {
+
+		try {
+			broker.bind(ISyncService.class.getSimpleName(), syncService);
+			logger.info("SyncService is ready and waiting for messages...");
+		} catch (Exception e) {
+			logger.fatal("Could not bind queue.", e);
+			System.exit(5);
+		}
+
+		logger.info("Initializing XML RPC...");
+		try {
+			launchXmlRpc();
+			logger.info("XML RPC initialization succeded");
+		} catch (Exception e) {
+			logger.fatal("Could not initialize XMLRPC.", e);
+			System.exit(6);
+		}
+	}
+
+	@Override
+	public void stop() throws Exception {
+		try {
+			broker.stopBroker();
+		} catch (Exception e) {
+			logger.fatal("Error stoping SyncService.", e);
+			throw e;
+		}
+	}
+	
+	@Override
+	public void destroy() {
+		broker = null;
+	}
+
+	private static void launchXmlRpc() throws Exception {
+		xmlRpcServer = new XmlRpcSyncServer(Constants.XMLRPC_PORT);
+		xmlRpcServer.addHandler("XmlRpcSyncHandler", new XmlRpcSyncHandler(
+				broker, pool));
+		xmlRpcServer.serve_forever();
+	}
+
+	private static String getVersion() {
+		String path = "/version.properties";
+		InputStream stream = Config.class.getResourceAsStream(path);
+		if (stream == null) {
+			return "UNKNOWN";
+		}
+		Properties props = new Properties();
+		try {
+			props.load(stream);
+			stream.close();
+			return (String) props.get("version");
+		} catch (IOException e) {
+			return "UNKNOWN";
+		}
+	}
+}
