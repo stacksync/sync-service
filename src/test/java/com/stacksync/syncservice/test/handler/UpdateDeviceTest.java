e15fc2f164dc253ba57d29f9a303d790ad77a7e7
Adrian Moreno <adrian.moreno@urv.cat>
2014-01-13 13:01:03 +0100

    fixed device unit testing

--- src/test/java/com/stacksync/syncservice/test/handler/UpdateDeviceTest.java
+++ src/test/java/com/stacksync/syncservice/test/handler/UpdateDeviceTest.java
@@ -26,7 +26,8 @@ public class UpdateDeviceTest {
 	private static Handler handler;
 	private static WorkspaceDAO workspaceDAO;
 	private static UserDAO userDao;
-	private static User user;
+	private static User user1;
+	private static User user2;
 
 	@BeforeClass
 	public static void initializeData() {
@@ -35,7 +36,8 @@ public class UpdateDeviceTest {
 			Config.loadProperties();
 
 			String datasource = Config.getDatasource();
-			ConnectionPool pool = ConnectionPoolFactory.getConnectionPool(datasource);
+			ConnectionPool pool = ConnectionPoolFactory
+					.getConnectionPool(datasource);
 
 			handler = new SQLHandler(pool);
 			DAOFactory factory = new DAOFactory(datasource);
@@ -45,16 +47,26 @@ public class UpdateDeviceTest {
 			workspaceDAO = factory.getWorkspaceDao(connection);
 			userDao = factory.getUserDao(connection);
 
-			user = new User(null, "junituser", "aa", "aa", 1000, 100);
-			try{
-			userDao.add(user);
+			user1 = new User(null, "junituser", "aa", "aa", 1000, 100);
+			try {
+				userDao.add(user1);
 				Workspace workspace1 = new Workspace(null, "junituser1/", 1,
-						user);
+						user1);
 				workspaceDAO.add(workspace1);
-			}catch(DAOException e)
-			{
+			} catch (DAOException e) {
 				System.out.println("User already exists.");
-				user = userDao.findByCloudId("aa");
+				user1 = userDao.findByCloudId("aa");
+			}
+			
+			user2 = new User(null, "junituser", "bb", "bb", 1000, 100);
+			try {
+				userDao.add(user2);
+				Workspace workspace1 = new Workspace(null, "junituser1/", 1,
+						user2);
+				workspaceDAO.add(workspace1);
+			} catch (DAOException e) {
+				System.out.println("User already exists.");
+				user2 = userDao.findByCloudId("bb");
 			}
 
 		} catch (Exception e) {
@@ -64,77 +76,74 @@ public class UpdateDeviceTest {
 
 	@AfterClass
 	public static void cleanData() throws DAOException {
-		//userDao.delete("aa");
+		// userDao.delete("aa");
 	}
 
 	@Test
 	public void registerNewDevice() throws DAOException {
 
-		
 		Device device = new Device();
-		device.setUser(user);
+		device.setUser(user1);
 		device.setName("john's computer");
 		device.setOs("Linux");
 		device.setLastIp("15.26.156.98");
 		device.setAppVersion("1.2.3");
-		
+
 		Long result = handler.doUpdateDevice(device);
 
 		System.out.println("Result: " + result + " | Device: " + device);
 
 		assertNotEquals(-1L, result.longValue());
 	}
-	
+
 	@Test
 	public void updateExistingDevice() throws DAOException {
 
-		
 		Device device = new Device();
-		device.setUser(user);
+		device.setUser(user1);
 		device.setName("john's computer");
 		device.setOs("Linux");
 		device.setLastIp("15.26.156.98");
 		device.setAppVersion("1.2.3");
-		
+
 		Long result1 = handler.doUpdateDevice(device);
 
 		System.out.println("Result: " + result1 + " | Device: " + device);
-		
-		user.setId(999L);
+
+		assertNotEquals(-1L, result1.longValue());
+
 		device.setLastIp("1.1.1.1");
 		device.setAppVersion("3.3.3");
-		
+
 		Long result2 = handler.doUpdateDevice(device);
 		System.out.println("Result: " + result2 + " | Device: " + device);
-		
+
 		assertEquals(result1.longValue(), result2.longValue());
 	}
-	
+
 	@Test
 	public void updateAlienDevice() throws DAOException {
 
-		
 		Device device = new Device();
-		device.setUser(user);
+		device.setUser(user1);
 		device.setName("john's computer");
 		device.setOs("Linux");
 		device.setLastIp("15.26.156.98");
 		device.setAppVersion("1.2.3");
-		
-		Long result= handler.doUpdateDevice(device);
+
+		Long result = handler.doUpdateDevice(device);
 
 		System.out.println("Result: " + result + " | Device: " + device);
-		
-		user.setId(999999999L);
+
+		device.setUser(user2);
 		device.setLastIp("1.1.1.1");
 		device.setAppVersion("3.3.3");
-		
+
 		result = handler.doUpdateDevice(device);
-		
+
 		System.out.println("Result: " + result + " | Device: " + device);
-		
+
 		assertEquals(-1L, result.longValue());
 	}
-	
 
 }
