0dd4bbd7784e099adc292b5d00db2445acd3eca5
sergitoda <sergi.toda@estudiants.urv.cat>
2015-03-27 12:38:34 +0100

    SyncServer tested using both tests and StackSync client.

--- src/test/java/com/stacksync/syncservice/test/main/ServerTest.java
+++ src/test/java/com/stacksync/syncservice/test/main/ServerTest.java
@@ -21,7 +21,7 @@ public class ServerTest {
 				
 				@Override
 				public String[] getArguments() {
-					return new String[]{"/home/edgar/Documents/stackSync/sync-service/config.properties"};
+					return new String[]{"config2.properties"};
 				}
 			};
 			
